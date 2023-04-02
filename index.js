cf4b6bb82fddc4ae24f8297d10d5e3f5d70ddf71
Ely Novaes <elynnovaes96@gmail.com>
2023-04-02 15:03:07 -0300

    step2

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const info = Date()
+
+  response.send(`<p>Phonebook has info for ${persons.length} persons</p><p>${info}</>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
