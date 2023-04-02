4843aa6a816e4917d52d8feae1819d92d12d5af4
Ely Novaes <elynnovaes96@gmail.com>
2023-04-02 15:06:33 -0300

    step3

--- index.js
+++ index.js
@@ -36,6 +36,18 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${persons.length} persons</p><p>${info}</>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
