7a8df38df9e20435218fb0b52ee8d88dae67a34d
Ely Novaes <elynnovaes96@gmail.com>
2023-04-02 16:44:44 -0300

    step6

--- index.js
+++ index.js
@@ -63,6 +63,16 @@ const generateId = (max, min) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number is missing'
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
   const person ={
     id: generateId(1000000, 10),
     name: body.name,
