e02ffa61fc580a7eadd558bcee7b93b13efdf3da
Ely Novaes <elynnovaes96@gmail.com>
2023-04-17 00:17:10 -0300

    step7

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ const morganInfo = (tokens, req, res) => {
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if(error === 'CastError') {
+  if(error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -71,7 +73,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (body === undefined) {
@@ -83,9 +85,11 @@ app.post('/api/persons', (request, response) => {
     number: body.number
   })
 
-  person.save().then(savedPerson =>{
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson =>{
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
