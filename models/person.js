a82e1163fd4bb0b4fcf53e666f8ec4d969c0bcbf
Ely Novaes <elynnovaes96@gmail.com>
2023-04-18 10:17:36 -0300

    step8

--- models/person.js
+++ models/person.js
@@ -18,7 +18,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String
+  number: {
+    type: String,
+    required: true,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /\d{3}-\d{7}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number`
+    }
+  }
 })
 
 mongoose.set('toJSON', {
